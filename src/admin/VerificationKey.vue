2859c498187f014f2d6fdaa69e79301faff2c287
natnael <melakunati01@gmail.com>
2021-02-06 10:21:47 +0300

    verification key generated

--- src/admin/VerificationKey.vue
+++ src/admin/VerificationKey.vue
@@ -1,11 +1,132 @@
 <template>
     <div>
-        <h1>hhhhhhhhhh</h1>
+
+        <h3 class="text-center">Generatr Verification Key</h3>
+         
+        
+    <b-form-select style="margin-top:10px" v-model="selected" :options="options"></b-form-select>
+    <div>
+        <p style="color:red" v-if="notSelected">
+            You Have To Select Workbook
+        </p>
     </div>
+    <CInput
+              label="Number Of Verification Code"
+              type ="number"
+              v-model="numberOfCodes"
+              style="margin-top:10px"
+              horizontal
+              placeholder=""
+            />
+
+            <p style="color:red" v-if="noNotSelected">
+            Number Of Verification Code Can't Be Empity
+        </p>
+
+
+    <b-button style="float:right" variant="success" @click="submit()">submit</b-button>
+    <div v-if="verification.length">
+           <div id="printMe">
+<table class="table" >
+
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">WorkBook</th>
+                    <th scope="col">Key</th>
+                    
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for=" (verification,index) in verification" :key="index">
+                    <th>{{index+1}}</th>
+                    <th>
+                        <p v-if="verification.selected == 1">1</p>
+                        <p v-if="verification.selected == 2"> 1&2</p>
+                        <p v-if="verification.selected == 3">1,2&3 </p>
+                        <p v-if="verification.selected == 4">All 4</p>
+                    </th>
+                    <th style="font-size:2">{{verification.code}}</th>
+`               </tr>
+            </tbody>
+            
+        </table>
+    </div> 
+    <p style="text-alignment : center">
+    <b-button name @click="print" variant="success">print</b-button>
+
+    </p>
+    </div>
+
+            
+    </div>
+    
 </template>
 <script>
+import Vue from 'vue';
+import VueHtmlToPaper from 'vue-html-to-paper';
+const options = {
+  name: '_blank',
+  specs: [
+    'fullscreen=yes',
+    'titlebar=yes',
+    'scrollbars=yes'
+  ],
+  
+  styles: [
+'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+  ]
+}
+Vue.use(VueHtmlToPaper, options);
 export default {
-    
+    data() {
+      return {
+          verification:[],
+          notSelected:false,
+          noNotSelected: false,
+        numberOfCodes:null,
+        selected: null,
+        options: [
+            { value: null, text: 'Please select Workbook' },
+          { value: 1, text: 'Workbook 1' },
+          { value: 2, text: 'Workbook 1 & 2' },
+          { value: 3, text: 'Workbook 1 & 2 & 3' },
+          { value: 4, text: 'all 4 Workbooks' }
+        ],
+
+      }
+    },
+    methods: {
+        submit(){
+            
+            if(!this.selected){
+                this.notSelected = true;
+            }
+            if(!this.numberOfCodes){
+                this.noNotSelected = true;
+            }
+            if(this.selected && this.numberOfCodes){
+                console.log("tests");
+                this.notSelected = false;
+                this.noNotSelected = false;
+                
+                for(var i = 0; i < this.numberOfCodes; i++){
+                    const today = new Date();
+                    var timestamp =today.valueOf().toString();
+                    var rand = Math.round((Math.random()*100)).toString();
+                    console.log(timestamp);
+                    console.log(rand);
+                    var key = timestamp + rand;
+                    this.verification.push({selected: this.selected,code : key});
+                }
+            }
+        },
+            print () {
+      // Pass the element id here
+      this.$htmlToPaper('printMe');
+    }
+
+    },
 }
 </script>
 <style>
