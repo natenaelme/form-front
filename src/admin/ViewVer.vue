d65d69fd51e011fd5d1862023c33a9500faa0f0f
natnael <melakunati01@gmail.com>
2021-02-19 17:02:27 +0300

    filter by date

--- src/admin/ViewVer.vue
+++ src/admin/ViewVer.vue
@@ -1,20 +1,37 @@
 <template>
   <div>
+    <b-jumbotron>
     <h3 class="text-center">View Verification Key</h3>
     <b-form-select
-      style="margin-top: 10px"
+      
       v-model="selected"
       :options="options"
     ></b-form-select>
     <div>
       <p style="color: red" v-if="notSelected">You Have To Select Workbook</p>
-    </div>
+    </div><br>
     <b-button style="float: right" variant="success" @click="ViewForm()"
-      >submit</b-button
-    >
-    <div v-if="verification.length">
+      >Submit</b-button>
       <br>
       <br>
+      <h6>Filter All WorkBook Verification Key By Date</h6>
+    <b-row>
+      <b-col>
+        <b-form-datepicker v-model="filterData.StartingDate" placeholder="Select Staring Date" ></b-form-datepicker>
+      </b-col>
+      <b-col>
+        <b-form-datepicker v-model="filterData.EndDate" placeholder="Select End Date" ></b-form-datepicker>
+      </b-col>
+    </b-row>
+    <p style="color:red" v-if="incorectDate">fill the date correctly to filter the Data</p>
+    <br>
+    <b-button style="float: right" @click="filterByDate()" variant="primary">Filter</b-button>
+</b-jumbotron>
+
+
+    <div v-if="!verification.length"><h5 style="color:green;text-align:center">there is no verification Key</h5></div>
+    <div v-if="verification.length && verification[0]!= ''">
+      
       <b-row>
         <b-col>
           <b-button variant="primary" @click="filter(true)">filter only used key</b-button>
@@ -28,7 +45,9 @@
 
         </b-col>
         <br>
+        <br>
       </b-row>
+      
       <div id="printMe">
         <table class="table">
           <thead>
@@ -37,6 +56,7 @@
               <th scope="col">WorkBook</th>
               <th scope="col">Key</th>
               <th>Used</th>
+              <th>Created Date</th>
             </tr>
           </thead>
           <tbody>
@@ -53,6 +73,9 @@
                 <p v-if="verification.used">Used</p>
                 <p v-if="!verification.used">Never Used</p>
               </th>
+              <th>
+                {{verification.Date}}
+              </th>
             </tr>
           </tbody>
         </table>
@@ -66,7 +89,7 @@
 </template>
 <script>
 import Vue from "vue";
-const { getterVerKey } = require("../assets/js/service");
+const { getterVerKey,getters,getterVerification} = require("../assets/js/service");
 import VueHtmlToPaper from "vue-html-to-paper";
 const options = {
   name: "_blank",
@@ -81,12 +104,19 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 export default {
   data() {
     return {
-      verification: [],
+      filterData:{
+        StartingDate:null,
+        EndDate:null
+      },
+      incorectDate:false,
+      value:null,
+      verification: [''],
       allverfi:[],
       selected: null,
       notSelected: false,
       options: [
         { value: null, text: "Please select Workbook" },
+        { value: 5, text:"all Verification Key"},
         { value: 1, text: "Workbook 1" },
         { value: 2, text: "Workbook 1 & 2" },
         { value: 3, text: "Workbook 1 & 2 & 3" },
@@ -95,11 +125,20 @@ export default {
     };
   },
   methods: {
+
     ViewForm() {
+      console.log(this.selected);
       if (!this.selected) {
         this.notSelected = true;
       }
       if (this.selected) {
+        if(this.selected == 5){
+          getters("verifications").then(resp=>{
+            console.log(resp.data);
+            this.verification = resp.data;
+          this.allverfi=resp.data;
+          })
+        }else{
         this.notSelected = false;
         getterVerKey("workbook",this.selected).then((resp) => {
           console.log(resp.data);
@@ -119,7 +158,33 @@ export default {
               }
             });
         
-      }
+      }}
+    },
+    filterByDate(){
+      let database = "users/getVerBnDate";
+      if(this.filterData.StartingDate && this.filterData.EndDate){
+        this.incorectDate = false;
+      getterVerification(database,localStorage.getItem('token'),this.filterData).then(resp=>{
+        console.log(resp);
+        this.verification = resp.data.responce;
+        this.allverfi=resp.data.responce;
+      }).catch((err) => {
+              if (err.response) {
+                // client received an error response (5xx, 4xx)
+                this.makeToast(
+                  "danger",
+                  "There is Some Error.Please Check your Form"
+                );
+              } else if (err.request) {
+                this.makeToast("danger", "Connection Problem");
+              } else {
+                this.makeToast("danger", "Some Error has Happened");
+              }
+            });}
+            else{
+              this.incorectDate = true;
+            }
+        
     },
     print() {
       // Pass the element id here
